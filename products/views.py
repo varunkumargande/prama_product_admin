47986b0a788eee06cfaf02042265ed58ccd54813
varun <babluvarun@gmail.com>
2022-01-11 00:37:18 +0530

    bug fix

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics, permissions, status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+# from rest_framework import authentication
 from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Product
@@ -12,7 +13,6 @@ class ProductViewset(ModelViewSet):
     permission_classes = [permissions.IsAuthenticated,]
     filter_backends = (DjangoFilterBackend,
                        filters.SearchFilter, filters.OrderingFilter, )
-    pagination_class = LimitOffsetPagination
     filter_fields = ('id', 'name',)
     ordering_fields = ('id', 'name',)
     search_fields = ('name',)
